16fdba954c5b8d046a7affa78fa40d6e326de155
chidiokoroafor <cokoroafor@healthstation.ng>
2024-11-28 17:33:26 +0100

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ function App() {
   return (
     <main className='bg-slate-300 w-full min-h-screen flex justify-center items-center'>
       <div className="w-[90%] max-w-[380px] p-4 bg-white rounded-xl">
-        <div className='rounded-xl overflow-hidden'>
+        <div className='rounded-xl overflow-hidden '>
           <img className='max-w-[100%]' src={qrcodeimg} alt='qr code' />
         </div>
         
-        <div className='mt-6 font-sans  text-center p-4'>
+        <div className='mt-3 font-sans  text-center p-4'>
           <h2 className='font-bold text-slate-900 text-2xl'>Improve your front-end skills by building projects</h2>
-          <p className='mt-4 font-normal text-lg text-slate-500 mb-6'>
+          <p className='mt-3 font-normal text-lg text-slate-500 mb-3'>
             Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
           </p>
         </div>
