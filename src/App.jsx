1284f03ef4c18cbc06bde1c3fc5e0916d4638737
chidiokoroafor <cokoroafor@healthstation.ng>
2024-11-28 17:04:35 +0100

    built the challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import qrcodeimg from './assets/images/image-qr-code.png'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className='bg-slate-300 w-full min-h-screen flex justify-center items-center'>
+      <div className="w-[90%] max-w-[380px] p-4 bg-white rounded-xl">
+        <div className='rounded-xl overflow-hidden'>
+          <img className='max-w-[100%]' src={qrcodeimg} alt='qr code' />
+        </div>
+        
+        <div className='mt-6 font-sans  text-center p-4'>
+          <h2 className='font-bold text-slate-900 text-2xl'>Improve your front-end skills by building projects</h2>
+          <p className='mt-4 font-normal text-lg text-slate-500 mb-6'>
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
+        </div>
+    </div>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': 'hsl(0, 0%, 100%)',
+        'slate': {
+          300: 'hsl(212, 45%, 89%)',
+          500: 'hsl(216, 15%, 48%)',
+          900: 'hsl(218, 44%, 22%)'
+        }
+      },
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
